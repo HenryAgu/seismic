a8d1013a831339c91aa86546436ad7f81f8f8f44
Henry Agu <aguhenrychuks@gmail.com>
2026-01-28 22:49:52 +0100

    feat: popular stores section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import PopularStores from "@/components/popular-stores";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <PopularStores/>
     </main>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="lg:pt-50 pt-30 flex flex-col gap-y-6 px-5">
+    <section className="lg:pt-50 pt-30 pb-12 flex flex-col gap-y-6 px-5">
       <div className="flex flex-col items-center w-full gap-y-6">
         <span className="bg-primary-50 border-[0.5px] border-primary-200 rounded-full py-3 px-6 w-fit text-sm font-normal custom-text-tracking">
           Global Shopping, Made Simple
@@ -20,7 +20,7 @@ const Hero = () => {
       />
       <div className="flex flex-col items-center text-center gap-y-6 max-w-141 mx-auto">
         <p className="text-base tracking-[0.5%] font-normal leading-[25.6px] text-grey-700">
-          We help{" "}
+          We help
           <span className="text-grey-950">Nigerians shop from abroad,</span>{" "}
           receive items at our overseas warehouse, and{" "}
           <span className="text-grey-950">ship them safely to Nigeria,</span>{" "}
