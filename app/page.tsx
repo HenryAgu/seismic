565464760397bd3b97730c061a33966d1b7e7110
Henry Agu <aguhenrychuks@gmail.com>
2026-01-29 13:10:34 +0100

    feat: steps

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import BuiltForYou from "@/components/built-for-you";
 import CreateAccount from "@/components/create-account";
 import Faqs from "@/components/faqs";
 import Hero from "@/components/hero";
+import HowItWorks from "@/components/how-it-works";
 import PlatformHighlights from "@/components/platform-highlights";
 import PopularStores from "@/components/popular-stores";
 import Services from "@/components/services";
@@ -11,6 +12,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <PopularStores />
+      <HowItWorks/>
       <PlatformHighlights />
       <BuiltForYou/>
       <Services />
---
+++ components/how-it-works.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./header";
+import SubText from "./sub-text";
+import Image from "next/image";
+
+const HowItWorks = () => {
+  return (
+    <section className="px-5 lg:px-10 xl:px-32 py-12 flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-6">
+        <Header title="Simple Steps" />
+        <SubText
+          title="How it Works"
+          descriptionClassName="max-w-[472px]"
+          description="Buying from international stores doesn’t have to be complicated. Here’s how we make it simple."
+        />
+      </div>
+      <div className="my-10 w-full lg:w-[80%] mx-auto">
+        <Image src="/steps.webp" width={967} height={1240} alt="steps-image" className="aspect-967/1240 w-full"/>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
